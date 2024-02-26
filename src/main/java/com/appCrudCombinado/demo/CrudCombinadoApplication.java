d4e691170b0ff40dbdef245879fdc3a04e7a808d
Daniel <daniel_mtz_ojeda@hotmail.com>
2024-02-25 23:00:25 -0600

    Inicializacion de proyectgo CRUD

---
+++ src/main/java/com/appCrudCombinado/demo/CrudCombinadoApplication.java
@@ -0,0 +1,13 @@
+package com.appCrudCombinado.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCombinadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCombinadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appCrudCombinado/demo/CrudCombinadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appCrudCombinado.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCombinadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
